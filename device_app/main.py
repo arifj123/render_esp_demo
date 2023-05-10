3302fa8a6ae0cc14a6e52d450e3ff27651ba13dc
arifj123 <114598727+arifj123@users.noreply.github.com>
2023-05-10 23:32:08 +0200

    Delete device_app directory

--- device_app/main.py
+++
@@ -1,6 +0,0 @@
-from dash import Dash
-from flask import Flask
-
-server = Flask(__name__)
-app = Dash(__name__, server = server, url_base_pathname = '/visualization/')
-app.title = "Visualization of Power Consumption"
